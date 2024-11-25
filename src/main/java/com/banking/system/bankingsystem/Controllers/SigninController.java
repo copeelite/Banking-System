297c733199dc64997db24e2e3db7acd020d08f0c
copeelite <chef777777777@gmail.com>
2024-11-25 11:32:16 -0500

    make sign up ui

--- src/main/java/com/banking/system/bankingsystem/Controllers/SigninController.java
+++ src/main/java/com/banking/system/bankingsystem/Controllers/SigninController.java
@@ -1,7 +1,11 @@
 package com.banking.system.bankingsystem.Controllers;
 
 import com.banking.system.bankingsystem.Models.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -32,5 +36,21 @@ public class SigninController implements Initializable {
         Stage stage = (Stage) error_lbl.getScene().getWindow();
         Model.getInstance().getViewFactory().closeStage(stage);
         Model.getInstance().getViewFactory().showClientWindow();
+    }
+
+    public void handleSignUp() {
+        loadFXML("/Fxml/Signup.fxml");
+    }
+
+    private void loadFXML(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+            Stage stage = (Stage) email_address_fld.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
